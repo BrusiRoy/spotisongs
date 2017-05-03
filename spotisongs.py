4e3313f1fd3bf52b9608ba89559abb42c7dd3efb
Bruno Roy <brusi_roy@hotmail.com>
2017-05-03 00:11:20 -0400

    Create a playlist and add the content of "Your Msuic" to it

--- spotisongs.py
+++ spotisongs.py
@@ -1,8 +1,21 @@
 import sys
+
 import spotipy
 import spotipy.util as util
 
-scope = 'user-library-read'
+scope = 'user-library-read playlist-modify-public'
+READ_LIMIT = 50
+
+def create_playlist():
+    # Creating the new playlist (delete the old one if present)
+    playlists = sp.user_playlists(username)
+    playlist_name = username + "'s Music"
+    for playlist in playlists['items']:
+        if (playlist['name'] == playlist_name):
+            sp.user_playlist_unfollow(username, playlist['id'])
+    # Return the playlist id of the newly created playlist
+    return sp.user_playlist_create(username, playlist_name)['id']
+
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -14,15 +27,16 @@ token = util.prompt_for_user_token(username, scope)
 
 if token:
     sp = spotipy.Spotify(auth=token)
+    playlist_id = create_playlist()
+
     offset = 0
-    results = sp.current_user_saved_tracks(50, offset)
-    print results
-    print len(results['items']);
+    results = sp.current_user_saved_tracks(READ_LIMIT, offset)
     while len(results['items']) > 0:
+        tracks = list()
         for item in results['items']:
-            track = item['track']
-            print track['name'] + ' - ' + track['artists'][0]['name']
-        offset += 50
-        results = sp.current_user_saved_tracks(50, offset)
+            tracks.append(item['track']['uri'])
+        sp.user_playlist_add_tracks(username, playlist_id, tracks)
+        offset += READ_LIMIT
+        results = sp.current_user_saved_tracks(READ_LIMIT, offset)
 else:
     print "Can't get token for", username
